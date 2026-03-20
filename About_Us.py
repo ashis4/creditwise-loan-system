da2d38101af50ef9e34e6f1f273f2732dd5bb4cc
Ashish Ramchandra Gaikar <ashishgaikar2006@gmail.com>
2026-03-20 21:32:40 +0530

    Delete About_Us.py

--- About_Us.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="About CreditWise", layout="wide")
-
-def load_html(file_name):
-    with open(file_name, encoding="utf-8") as f:
-        return f.read()
-
-st.markdown(load_html("about.html"), unsafe_allow_html=True)
