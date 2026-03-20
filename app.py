cab5bbc0e453a95845f0a28476aae57e8e14fe75
Ashish Ramchandra Gaikar <ashishgaikar2006@gmail.com>
2026-03-20 19:46:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ st.markdown("""
     </a>
 </div>
 
-<hr style="border:0.5px solid #374151; margin-top: 10px; margin-bottom: 4px;">
+<hr style="border:0.5px solid #374151; margin-top: 15px; margin-bottom: 4px;">
 
 <div style="text-align:center; font-size:13px; color:#6b7280; margin:0;">
     © 2026 CreditWise. All rights reserved.
